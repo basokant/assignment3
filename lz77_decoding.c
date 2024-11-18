c7ccd387706cd7f05adbb85134c37e0d3aa74e63
basokant <basokanthan@gmail.com>
2024-11-17 20:16:41 -0500

    add decompression time

--- lz77_decoding.c
+++ lz77_decoding.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "lz77_decoding_function.h"
 
@@ -9,6 +10,17 @@ int main(int argc, char *argv[]) {
         exit(0);
     }
 
+    struct timeval start;
+    struct timeval end;
+    unsigned long e_usec = 0;  // elapsed compression time in microseconds
+
+    gettimeofday(&start, 0);
+
     char *input_lz_encoded_file_name = argv[1];
     Decode_Using_LZ77(input_lz_encoded_file_name);
+
+    gettimeofday(&end, 0);
+    e_usec = ((end.tv_sec * 1000000) + end.tv_usec) -
+             ((start.tv_sec * 1000000) + start.tv_usec);
+    printf("Decompression Time: %lu microseconds\n", e_usec);
 }
