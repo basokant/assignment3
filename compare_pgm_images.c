3a799c7403911405ed39c62faa2cd60d48e53850
basokant <basokanthan@gmail.com>
2024-11-17 19:05:45 -0500

    finish compare_pgm_images

--- compare_pgm_images.c
+++ compare_pgm_images.c
@@ -1,1 +1,15 @@
-int main(int argc, char *argv[]) {}
+#include "mean_absolute_error.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Expected 2 arguments. Received %d\n", argc - 1);
+        exit(0);
+    }
+
+    char *first_image_name = argv[1];
+    char *second_image_name = argv[2];
+
+    float mae = mean_absolute_error(first_image_name, second_image_name);
+    printf("mean absolute error = %f\n", mae);
+    return mae;
+}
